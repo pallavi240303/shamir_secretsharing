dbb7fdc69e30ad87cac9b908f4e90c3d8ea097cd
pallavi240303 <24.pallavi.2003@gmail.com>
2024-10-25 16:21:14 +0530

    Chore: SSS and VSS Implementation

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,41 @@
+use sss::{convert_from_decimal, generate_commitments, parse_shares, recover_secret};
 
 mod sss;
-mod utils;
-use sss::{Charset, SecretSharing};
-use std::process::exit;
 
-fn main() {
-    let secret = "supersecretmessage";
-    let mut secret_sharing = SecretSharing::new(3, 8, Charset::Alphanumeric);
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let input = r#"{
+        "keys": {"n": 10, "k": 7},
+        "1": {"base": "6", "value": "13444211440455345511"},
+        "2": {"base": "15", "value": "aed7015a346d63"},
+        "3": {"base": "15", "value": "6aeeb69631c227c"},
+        "4": {"base": "16", "value": "e1b5e05623d881f"},
+        "5": {"base": "8", "value": "316034514573652620673"},
+        "6": {"base": "3", "value": "2122212201122002221120200210011020220200"},
+        "7": {"base": "3", "value": "20120221122211000100210021102001201112121"},
+        "8": {"base": "6", "value": "20220554335330240002224253"},
+        "9": {"base": "12", "value": "45153788322a1255483"},
+        "10": {"base": "7", "value": "1101613130313526312514143"}
+    }"#;
 
-    match secret_sharing.generate_shares(secret) {
-        Ok(shares) => {
-            println!("Shares:");
-            for (i, share) in shares.iter().enumerate() {
-                println!("Share {}: {}", i + 1, share);
-            }
+    let shares = parse_shares(input)?;
+    let k = 7; 
 
-            let reconstruction_shares = &shares[0..3];
-            match secret_sharing.reconstruct_secret(reconstruction_shares) {
-                Ok(reconstructed_secret) => {
-                    println!("Reconstructed secret: {}", reconstructed_secret);
-                }
-                Err(e) => {
-                    eprintln!("Error reconstructing secret: {:?}", e);
-                    exit(1);
-                }
-            }
-        }
-        Err(e) => {
-            eprintln!("Error generating shares: {:?}", e);
-            exit(1);
+    if let Some(secret) = recover_secret(&shares, k) {
+        println!("Recovered secret: {}", secret);
+
+        println!("Secret in base 16: {}", convert_from_decimal(secret.clone(), 16));
+        println!("Secret in base 10: {}", secret);
+        println!("Secret in base 2: {}", convert_from_decimal(secret, 2));
+
+        let poly = vec![3, 5, 2]; 
+
+        let commitments = generate_commitments(&poly);
+
+        println!("Commitments:");
+        for (i, commitment) in commitments.iter().enumerate() {
+            println!("Commitment for coefficient {}: {}", i, commitment);
         }
     }
+
+    Ok(())
 }
--- src/sss.rs
+++ src/sss.rs
@@ -1,102 +1,229 @@
-use crate::utils;
-use crate::utils::SSError;
-use num::BigInt;
 
-pub struct SecretSharing {
-    threshold: u64,
-    total: u64,
-    charset: Charset,
-    prime: Option<BigInt>,
+use num_bigint::BigInt;
+use num_traits::{One, Zero, ToPrimitive};
+
+const G: i128 = 3; // Replace with the actual generator value
+const P: i128 = 170141183460469231731687303715884105727; // Replace with the actual prime modulus
+
+#[derive(Debug, Clone)]
+pub struct Rational {
+    pub num: BigInt,
+    pub den: BigInt,
 }
 
-impl SecretSharing {
-    pub fn new(threshold: u64, total: u64, charset: Charset) -> Self {
-        SecretSharing {
-            threshold,
-            total,
-            charset,
-            prime: None,
+#[derive(Debug)]
+pub struct Share {
+    pub base: u32,
+    pub value: String,
+    pub x: BigInt,
+    pub y: BigInt,
+}
+
+impl Rational {
+    pub fn new(n: BigInt, d: BigInt) -> Self {
+        Self { num: n, den: d }.reduce()
+    }
+
+    fn reduce(self) -> Self {
+        let gcd = gcd(&self.num, &self.den);
+        Self {
+            num: self.num / gcd.clone(),
+            den: self.den / gcd,
         }
-    }
-
-    #[inline]
-    pub fn threshold(&self) -> u64 {
-        self.threshold
-    }
-
-    #[inline]
-    pub fn total(&self) -> u64 {
-        self.total
-    }
-
-    #[inline]
-    pub fn charset(&self) -> &str {
-        self.charset.charset_str()
-    }
-
-    #[inline]
-    fn set_prime(&mut self, prime: BigInt) {
-        self.prime = Some(prime)
-    }
-
-    #[inline]
-    pub fn prime(&self) -> Result<&BigInt, SSError> {
-        self.prime.as_ref().ok_or(SSError::PrimeNotSet)
-    }
-
-    pub fn generate_shares(&mut self, secret: &str) -> Result<Vec<String>, SSError> {
-        if self.threshold() < 2 {
-            return Err(SSError::LowThreshold);
-        }
-        if self.threshold() > self.total() {
-            return Err(SSError::HighThreshold);
-        }
-        let secret_int = utils::charset_repr_to_int(secret, self.charset())?;
-        self.set_prime(utils::next_prime(&secret_int)?);
-
-        let points =
-            utils::secret_int_to_points(secret_int, self.threshold(), self.total(), self.prime()?);
-
-        let shares: Result<Vec<_>, SSError> = points
-            .iter()
-            .map(|point| utils::point_to_share_str(point, self.charset()))
-            .collect();
-        shares
-    }
-
-    pub fn reconstruct_secret(&self, shares: &[String]) -> Result<String, SSError> {
-
-        if (shares.len() as u64) < self.threshold() {
-            return Err(SSError::InsufficientShares);
-        }
-
-        let point_shares: Result<Vec<_>, SSError> = shares
-            .iter()
-            .map(|share| utils::share_str_to_point(share.as_str(), self.charset()))
-            .collect();
-
-        let secret_int = utils::points_to_secret_int(point_shares?, self.prime()?)?;
-        utils::int_to_charset_repr(secret_int, self.charset())
     }
 }
 
-/// Possible charsets for secret.
-pub enum Charset {
-    Hex,
-    Alphanumeric,
-    Base58,
+impl std::ops::Mul for Rational {
+    type Output = Rational;
+
+    fn mul(self, other: Rational) -> Rational {
+        Rational::new(self.num * other.num, self.den * other.den).reduce()
+    }
 }
 
-impl Charset {
-    pub fn charset_str(&self) -> &str {
-        match self {
-            Charset::Hex => "0123456789abcdef",
-            Charset::Alphanumeric => {
-                "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+impl std::ops::Add for Rational {
+    type Output = Rational;
+
+    fn add(self, other: Rational) -> Rational {
+        Rational::new(
+            &self.num * &other.den + &self.den * &other.num,
+            &self.den * &other.den,
+        )
+        .reduce()
+    }
+}
+
+fn gcd(a: &BigInt, b: &BigInt) -> BigInt {
+    let mut a = a.clone();
+    let mut b = b.clone();
+    while !b.is_zero() {
+        let t = b.clone();
+        b = &a % &b;
+        a = t;
+    }
+    a
+}
+
+fn convert_to_decimal(value: &str, base: u32) -> Option<BigInt> {
+    let mut result = BigInt::zero();
+    let base_big = BigInt::from(base);
+
+    for c in value.chars() {
+        let digit = match c {
+            '0'..='9' => (c as u32 - '0' as u32),
+            'a'..='f' => (c as u32 - 'a' as u32 + 10),
+            'A'..='F' => (c as u32 - 'A' as u32 + 10),
+            _ => return None,
+        };
+
+        if digit >= base {
+            return None;
+        }
+
+        result = result * &base_big + BigInt::from(digit);
+    }
+
+    Some(result)
+}
+
+
+pub fn convert_from_decimal(mut num: BigInt, base: u32) -> String {
+    if num.is_zero() {
+        return "0".to_string();
+    }
+
+    let base_big = BigInt::from(base);
+    let mut result = String::new();
+
+    while !num.is_zero() {
+        let digit = (&num % &base_big).to_u32().unwrap();
+        let char = match digit {
+            0..=9 => (digit as u8 + b'0') as char,
+            10..=35 => ((digit - 10) as u8 + b'a') as char,
+            _ => panic!("Invalid digit"),
+        };
+        result.push(char);
+        num /= &base_big;
+    }
+
+    result.chars().rev().collect()
+}
+
+pub fn parse_shares(input: &str) -> Result<Vec<Share>, Box<dyn std::error::Error>> {
+    let data: serde_json::Value = serde_json::from_str(input)?;
+    let mut shares = Vec::new();
+
+    
+    let n = data["keys"]["n"].as_u64().unwrap() as usize;
+    let k = data["keys"]["k"].as_u64().unwrap() as usize;
+
+    for i in 1..=n {
+        let key = i.to_string();
+        if let Some(share) = data.get(&key) {
+            let base = share["base"].as_str().unwrap().parse::<u32>()?;
+            let value = share["value"].as_str().unwrap();
+
+            // Convert value from its base to decimal
+            if let Some(y) = convert_to_decimal(value, base) {
+                shares.push(Share {
+                    base,
+                    value: value.to_string(),
+                    x: BigInt::from(i),
+                    y,
+                });
             }
-            Charset::Base58 => {
-                "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+        }
+    }
+
+    Ok(shares)
+}
+
+pub fn generate_secret(x: &[BigInt], y: &[BigInt], m: usize) -> BigInt {
+    let mut ans = Rational::new(BigInt::zero(), BigInt::one());
+
+    for i in 0..m {
+        let mut l = Rational::new(y[i].clone(), BigInt::one());
+        for j in 0..m {
+            if i != j {
+                let temp = Rational::new(-x[j].clone(), &x[i] - &x[j]);
+                l = l * temp;
             }
         }
+        ans = ans + l;
+    }
+
+    ans.num
+}
+
+pub fn recover_secret(shares: &[Share], k: usize) -> Option<BigInt> {
+    if shares.len() < k {
+        println!("Not enough shares to recover the secret.");
+        return None;
+    }
+
+    let x: Vec<BigInt> = shares.iter().take(k).map(|s| s.x.clone()).collect();
+    let y: Vec<BigInt> = shares.iter().take(k).map(|s| s.y.clone()).collect();
+
+    Some(generate_secret(&x, &y, k))
+}
+
+
+pub fn generate_commitments(poly: &Vec<i128>) -> Vec<i128> {
+    let mut commitments = Vec::new();
+    for &coeff in poly.iter() {
+        // Compute g^coeff mod P as the commitment
+        let commitment = mod_exp(G, coeff, P);
+        commitments.push(commitment);
+    }
+    commitments
+}
+
+pub fn mod_exp(base: i128, exp: i128, modulus: i128) -> i128 {
+    let mut result = 1;
+    let mut base = base % modulus;
+    let mut exp = exp;
+
+    while exp > 0 {
+        if exp % 2 == 1 {
+            result = (result * base) % modulus;
+        }
+        exp >>= 1; 
+        base = (base * base) % modulus; 
+    }
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_base_conversion() {
+        let test_value = "aed7015";
+        let base = 15;
+        let decimal = convert_to_decimal(test_value, base).unwrap();
+
+        let result = convert_from_decimal(decimal, base);
+
+        let normalized_test = test_value.trim_start_matches('0');
+        let normalized_result = result.trim_start_matches('0');
+
+        assert_eq!(normalized_test, normalized_result);
+    }
+
+    #[test]
+    fn test_share_recovery() {
+        let json_input = r#"{
+            "keys": {"n": 3, "k": 2},
+            "1": {"base": "16", "value": "ff"},
+            "2": {"base": "16", "value": "1fe"},
+            "3": {"base": "16", "value": "2fd"}
+        }"#;
+
+        let shares = parse_shares(json_input).unwrap();
+        let secret = recover_secret(&shares, 2).unwrap();
+
+        let hex_secret = convert_from_decimal(secret, 16);
+        assert_eq!(hex_secret, "0"); 
     }
 }
